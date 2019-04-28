251fd062873868c1280ac315ed65abbb28244b10
Dibwe Kalangu <dleannykalangu@gmail.com>
2019-04-28 18:47:35 +0200

    rebuilding site Sun Apr 28 18:47:34 SAST 2019

---
+++ public/3-workshops/redux_intro/helloworld/use.js
@@ -0,0 +1,11 @@
+// To run this: node -r esm use.js
+
+import { store, increment, decrement } from "./index.js";
+
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
